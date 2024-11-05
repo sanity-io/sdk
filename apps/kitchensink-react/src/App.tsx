fca2e14ea1765a2578392984ef5f96d4a300177c
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-11-05 10:47:07 -0800

    chore: add kitchensink app with sdk aliases (#19)

---
+++ apps/kitchensink-react/src/App.tsx
@@ -0,0 +1,10 @@
+import {testFunction} from 'sdk-core'
+
+export function App(): JSX.Element {
+  testFunction()
+  return (
+    <div>
+      <h1>React Kitchensink</h1>
+    </div>
+  )
+}
---
+++ apps/kitchensink-react/vite.config.ts
@@ -0,0 +1,14 @@
+import {resolve} from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'sdk-core': resolve(import.meta.dirname, '../../packages/core/src'),
+      'sdk-react': resolve(import.meta.dirname, '../../packages/react/src'),
+    },
+  },
+})
