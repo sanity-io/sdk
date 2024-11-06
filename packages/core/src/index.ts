0d7e5fb6980086936505db6dd344942017b7753b
Ryan Bonial <ryanbonial@gmail.com>
2024-11-06 12:55:42 -0700

    chore: add example comment of vanilla subscription

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,9 @@
 import {sanitySdkStore} from './sanitySdkStore'
 
 // Example of a core vanilla js function that subscribes to the schema state
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const schemaTestSubscribe = (): void => {
-  // eslint-disable-next-line no-console, @typescript-eslint/no-explicit-any
-  sanitySdkStore.subscribe((state: any) => console.log('[+] Schema State updated', state.schema))
-}
+// const schemaTestSubscribe = (): void => {
+//   sanitySdkStore.subscribe((state: any) => console.log('[+] Schema State updated', state.schema))
+// }
 
 export {testFunction} from './example/example'
 export {sanitySdkStore}
