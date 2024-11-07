90d03c22cb85ef0ebaa748f70f857985c14fbc4a
Ryan Bonial <4294665+ryanbonial@users.noreply.github.com>
2024-11-07 11:05:36 -0700

    chore(core): add Zustand and example implementation (#27)
    * chore(core): add Zustand
    * chore: add example comment of vanilla subscription
    * chore: add useSanitySdk react example to kitchen sink

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,9 @@
+import {sanitySdkStore} from './sanitySdkStore'
+
+// Example of a core vanilla js function that subscribes to the schema state
+// const schemaTestSubscribe = (): void => {
+//   sanitySdkStore.subscribe((state: any) => console.log('[+] Schema State updated', state.schema))
+// }
+
 export {testFunction} from './example/example'
+export {sanitySdkStore}
