2f9b10a11610a6971f57aa6e9f21ed553c5bd1c6
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-11-14 14:02:10 -0800

    chore: add more tsdoc tags to package config

--- packages/core/package.config.ts
+++ packages/core/package.config.ts
@@ -1,7 +1,7 @@
-import baseConfig from '@repo/package.config'
+import {basePackageConfig} from '@repo/package.config'
 import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
-  ...baseConfig,
+  ...basePackageConfig,
   tsconfig: 'tsconfig.dist.json',
 })
