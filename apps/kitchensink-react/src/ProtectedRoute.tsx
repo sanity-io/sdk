a01d3b64b20e6e94156125933cf5715e13be866e
Binoy Patel <me@binoy.io>
2024-12-12 11:56:21 -0500

    fix: fixes issue with auth in kitchen-sink

--- apps/kitchensink-react/src/ProtectedRoute.tsx
+++ apps/kitchensink-react/src/ProtectedRoute.tsx
@@ -1,33 +1,35 @@
-import {AuthBoundary} from '@sanity/sdk-react/components'
-import {useCurrentUser, useLogOut} from '@sanity/sdk-react/hooks'
+import {useAuthState, useCurrentUser, useLogOut} from '@sanity/sdk-react/hooks'
 import {Avatar, Button} from '@sanity/ui'
-import {Link, Outlet} from 'react-router'
+import {Link, Navigate, Outlet} from 'react-router'
 
 export function ProtectedRoute(): JSX.Element {
+  const authState = useAuthState()
   const currentUser = useCurrentUser()
   const logout = useLogOut()
 
+  if (authState.type !== 'logged-in' || !currentUser) {
+    return <Navigate to="/" replace />
+  }
+
   return (
     <div style={{width: '100%', padding: '0 20px'}}>
-      <AuthBoundary>
-        <nav
-          style={{
-            height: '50px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Link to="/">Home</Link>
-          <div style={{display: 'flex', gap: 10, alignItems: 'center'}}>
-            <Avatar src={currentUser?.profileImage} color="blue" />
-            <span>{currentUser?.name}</span>
-            <Button onClick={() => logout()}>Logout</Button>
-          </div>
-        </nav>
+      <nav
+        style={{
+          height: '50px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Link to="/">Home</Link>
+        <div style={{display: 'flex', gap: 10, alignItems: 'center'}}>
+          <Avatar src={currentUser?.profileImage} color="blue" />
+          <span>{currentUser?.name}</span>
+          <Button onClick={() => logout()}>Logout</Button>
+        </div>
+      </nav>
 
-        <Outlet />
-      </AuthBoundary>
+      <Outlet />
     </div>
   )
 }
