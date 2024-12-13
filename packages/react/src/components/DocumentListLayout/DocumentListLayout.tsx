36dcd3595bd09eba3cd5e6bac57d9dfcd4fee035
Cole Peters <cole@colepeters.com>
2024-12-13 16:38:36 -0600

    feat(components): swap Sanity UI w/ CSS (doc collection) (#114)
    * feat(components): swap Sanity UI w/ CSS (doc collection):
    
    - Using @paramour/css to get going, we may want to replace this with
      something more purpose built, or at the very least modify the scales
      to use hand cranked values rather than generating them w/ ratios
    
    * feat(components): handle document preview media
    
    * feat(kitchensink): update DocumentPreview
    
    * feat(components): swap a for button on DocPreviewLayout

--- packages/react/src/components/DocumentListLayout/DocumentListLayout.tsx
+++ packages/react/src/components/DocumentListLayout/DocumentListLayout.tsx
@@ -1,15 +1,12 @@
-import {Stack} from '@sanity/ui'
+import '../../css/styles.css'
+
 import type {PropsWithChildren, ReactElement} from 'react'
 
 /**
  * @public
  */
 export const DocumentListLayout = (props: PropsWithChildren): ReactElement => {
-  return (
-    <Stack as="ol" data-ui="DocumentListLayout">
-      {props.children}
-    </Stack>
-  )
+  return <ol className="DocumentListLayout list-none">{props.children}</ol>
 }
 
 DocumentListLayout.displayName = 'DocumentListLayout'
--- packages/react/src/components/DocumentPreviewLayout/DocumentPreviewLayout.stories.tsx
+++ packages/react/src/components/DocumentPreviewLayout/DocumentPreviewLayout.stories.tsx
@@ -13,7 +13,6 @@ type Story = StoryObj<typeof meta>
 export const Basic: Story = {
   args: {
     title: 'Hello World',
-    url: '#',
   },
   render: (props) => {
     return <DocumentPreviewLayout {...props} />
@@ -24,7 +23,13 @@ export const AllProps: Story = {
   args: {
     title: 'Hello World',
     subtitle: 'It’s nice to meet you',
-    url: '#',
+    onClick: () => {
+      alert('Hello World!')
+    },
+    media: {
+      type: 'image-asset',
+      url: 'https://picsum.photos/80/80',
+    },
     docType: 'article',
     status: 'published',
   },
