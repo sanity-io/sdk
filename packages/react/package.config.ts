9610bb582c58df222106f39870d1b814978f4ea8
Ryan Bonial <ryanbonial@gmail.com>
2025-01-03 13:26:00 -0700

    chore: add rollup vizualizer plugin

--- packages/react/package.config.ts
+++ packages/react/package.config.ts
@@ -1,11 +1,24 @@
 import {basePackageConfig} from '@repo/package.config'
 import {defineConfig} from '@sanity/pkg-utils'
 import css from 'rollup-plugin-import-css'
+import visualizer from 'rollup-plugin-visualizer'
+
+const enableVisualizer = process.env['VISUALIZER'] === 'true'
 
 export default defineConfig({
   ...basePackageConfig,
   tsconfig: 'tsconfig.dist.json',
   rollup: {
-    plugins: [css()],
+    plugins: [
+      css(),
+      ...(enableVisualizer
+        ? [
+            visualizer({
+              filename: './stats/index.html',
+              open: false,
+            }),
+          ]
+        : []),
+    ],
   },
 })
