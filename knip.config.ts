2bf5f43d7da201a1475c0cecc0f08be30fdf5544
Binoy Patel <me@binoy.io>
2025-01-07 16:19:05 -0500

    chore: add depcheck script

---
+++ knip.config.ts
@@ -0,0 +1,52 @@
+import {KnipConfig} from 'knip'
+import {join} from 'node:path'
+import {match} from 'minimatch'
+
+const project = ['src/**/*.{js,jsx,ts,tsx}', '!**/build/**', '!**/docs/**']
+
+const baseConfig = {
+  workspaces: {
+    'apps/kitchensink-react': {
+      entry: ['src/main.tsx'],
+      project,
+    },
+    'packages/*': {
+      project,
+    },
+    'apps/*': {
+      project,
+    },
+  },
+} satisfies KnipConfig
+
+export const addBundlerEntries = async (config: KnipConfig) => {
+  const dirs = [
+    'packages/@repo/config-eslint',
+    'packages/@repo/package.config',
+    'packages/core',
+    'packages/react',
+    'apps/kitchensink-react',
+    'apps/storybook',
+  ]
+
+  for (const wsDir of dirs) {
+    for (const configKey of Object.keys(baseConfig.workspaces)) {
+      if (match([wsDir], configKey)) {
+        const manifest = await import(join(__dirname, wsDir, 'package.json'))
+        const configEntries = (config?.workspaces?.[configKey].entry as string[]) ?? []
+        const bundler = manifest?.bundler
+        for (const value of Object.values(bundler ?? {})) {
+          if (Array.isArray(value)) {
+            configEntries.push(...value)
+          }
+        }
+        if (config.workspaces && config.workspaces[configKey]) {
+          config.workspaces[configKey].entry = Array.from(new Set(configEntries))
+        }
+      }
+    }
+  }
+  return config
+}
+
+export default addBundlerEntries(baseConfig)
