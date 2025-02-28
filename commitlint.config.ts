e28ad1bfdf5b242aa42fb5afef97f6e2feff2115
Binoy Patel <me@binoy.io>
2025-02-28 14:11:51 -0500

    chore(renovate): use fix commit prefix for renovate

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,28 +1,7 @@
-import {RuleConfigSeverity, type UserConfig} from '@commitlint/types'
+import {type UserConfig} from '@commitlint/types'
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      RuleConfigSeverity.Error,
-      'always',
-      // Should match `.github/workflows/validate-pr-title.yml`
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-        'deps',
-      ],
-    ],
-  },
 }
 
 export default Configuration
