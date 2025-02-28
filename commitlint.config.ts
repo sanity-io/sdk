95eed18e67837af38b51bfdbfb7858e203117a6c
Binoy Patel <me@binoy.io>
2025-02-28 12:07:19 -0500

    chore: update commitlint to allow deps prefix

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+import {RuleConfigSeverity, type UserConfig} from '@commitlint/types'
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'deps',
+      ],
+    ],
+  },
+}
+
+export default Configuration
