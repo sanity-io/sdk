c72018664413a82bf4586b5ca62f82efeb530c14
Ryan Bonial <ryanbonial@gmail.com>
2025-03-06 11:45:25 -0700

    chore: use sanity resource id in sdk resource key

--- packages/core/src/resources/createResource.ts
+++ packages/core/src/resources/createResource.ts
@@ -76,15 +76,16 @@ export function getOrCreateResource<TState>(
   instance: SanityInstance,
   resource: Resource<TState>,
 ): InitializedResource<TState> {
+  const fullName = `${resource.name}-${instance.identity.resourceId}`
   if (!resourceCache.has(instance.identity)) {
     resourceCache.set(instance.identity, new Map())
   }
   const initializedResources = resourceCache.get(instance.identity)!
-  const cached = initializedResources.get(resource.name)
+  const cached = initializedResources.get(fullName)
   if (cached) return cached as InitializedResource<TState>
 
   const result = initializeResource(instance, resource)
-  initializedResources.set(resource.name, result)
+  initializedResources.set(fullName, result)
   return result
 }
 
