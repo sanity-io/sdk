2a2a4c079039fa77d71558365b387433e6a843ac
Ryan Bonial <4294665+ryanbonial@users.noreply.github.com>
2025-03-07 12:34:38 -0700

    Update packages/react/src/hooks/document/useDocumentSyncStatus.ts
    Co-authored-by: Cole Peters <cole@colepeters.com>

--- packages/react/src/hooks/document/useDocumentSyncStatus.ts
+++ packages/react/src/hooks/document/useDocumentSyncStatus.ts
@@ -8,7 +8,7 @@ type UseDocumentSyncStatus = {
    *
    * @category Documents
    * @param doc - The document handle to get sync status for. If you pass a `DocumentHandle` with a `resourceId` (in the format of `document:projectId.dataset:documentId`)
-   * the document will be read from the specified Sanity project and dataset that is included in the handle. If no `resourceId` is provided, the default project and dataset will be used.
+   * the document will be read from the specified Sanity project and dataset that is included in the handle. If no `resourceId` is provided, the default project and dataset from your `SanityApp` configuration will be used.
    * @returns `true` if local changes are synced with remote, `false` if the changes are not synced, and `undefined` if the document is not found
    * @example Disable a Save button when there are no changes to sync
    * ```
