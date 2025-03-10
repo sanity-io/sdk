b4da3eed0e08b96d49d69ca23ee871d370efd30f
Cole Peters <cole@colepeters.com>
2025-03-10 10:57:30 -0500

    docs: document useDatasets, useProjects, useProject; also cleanup (#292)

--- packages/react/src/hooks/projects/useProject.ts
+++ packages/react/src/hooks/projects/useProject.ts
@@ -8,8 +8,32 @@ import {
 
 import {createStateSourceHook} from '../helpers/createStateSourceHook'
 
+type UseProject = {
+  /**
+   *
+   * Returns metadata for a given project
+   *
+   * @category Projects
+   * @param projectId - The ID of the project to retrieve metadata for
+   * @returns The metadata for the project
+   * @example
+   * ```tsx
+   *  function ProjectMetadata({ projectId }: { projectId: string }) {
+   *    const project = useProject(projectId)
+   *
+   *    return (
+   *      <figure style={{ backgroundColor: project.metadata.color || 'lavender'}}>
+   *        <h1>{project.displayName}</h1>
+   *      </figure>
+   *    )
+   *  }
+   * ```
+   */
+  (projectId: string): SanityProject
+}
+
 /** @public */
-export const useProject = createStateSourceHook({
+export const useProject: UseProject = createStateSourceHook({
   // remove `undefined` since we're suspending when that is the case
   getState: getProjectState as (
     instance: SanityInstance,
