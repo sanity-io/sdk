911dca62f91620f1bf6ab3f962245ecfdfc9d12a
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2025-03-13 16:24:35 -0400

    feat(react): add comlink fetch hook

--- apps/kitchensink-react/src/Comlink/types.ts
+++ apps/kitchensink-react/src/Comlink/types.ts
@@ -1,9 +1,19 @@
-export type ToIFrameMessage = {
+export interface UserData {
+  id: string
+  name: string
+  email: string
+}
+
+export interface ToIFrameMessage {
   type: 'TO_IFRAME'
   data: {message: string}
 }
 
-export type FromIFrameMessage = {
+export interface FromIFrameMessage {
   type: 'FROM_IFRAME'
   data: {message: string}
+}
+
+export interface FetchUsersRequest {
+  type: 'FETCH_USERS'
 }
