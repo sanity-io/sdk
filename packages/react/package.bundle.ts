0cec9ab55e2bfe1dad9921f723d9d4f88964142a
Binoy Patel <me@binoy.io>
2025-04-03 12:38:43 -0400

    chore(build): add a bundle script to create auto updates bundle

---
+++ packages/react/package.bundle.ts
@@ -0,0 +1,14 @@
+import {defaultConfig} from '@repo/package.bundle'
+import {defineConfig, mergeConfig} from 'vite'
+
+export default defineConfig(() => {
+  return mergeConfig(defaultConfig, {
+    build: {
+      lib: {
+        entry: {
+          index: './src/_exports/index.ts',
+        },
+      },
+    },
+  })
+})
