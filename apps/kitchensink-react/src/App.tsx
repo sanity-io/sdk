46088dafe6afcf771ba376c767ef1f55efa40365
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2025-05-12 10:06:28 -0400

    feat(react): add navigate function

--- apps/kitchensink-react/src/App.tsx
+++ apps/kitchensink-react/src/App.tsx
@@ -1,8 +1,8 @@
-import {SanityApp, SanityConfig} from '@sanity/sdk-react'
+import {SanityApp, SanityConfig, useDashboardNavigate} from '@sanity/sdk-react'
 import {Spinner, ThemeProvider} from '@sanity/ui'
 import {buildTheme} from '@sanity/ui/theme'
-import {type JSX} from 'react'
-import {BrowserRouter} from 'react-router'
+import {type JSX, Suspense} from 'react'
+import {BrowserRouter, useNavigate} from 'react-router'
 
 import {AppRoutes} from './AppRoutes'
 
@@ -19,11 +19,22 @@ const sanityConfigs: SanityConfig[] = [
   },
 ]
 
+function NavigationHandler() {
+  const navigate = useNavigate()
+  useDashboardNavigate(({path, type}) => {
+    navigate(path, {replace: type === 'replace'})
+  })
+  return null
+}
+
 export default function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SanityApp fallback={<Spinner />} config={sanityConfigs}>
         <BrowserRouter>
+          <Suspense>
+            <NavigationHandler />
+          </Suspense>
           <AppRoutes />
         </BrowserRouter>
       </SanityApp>
