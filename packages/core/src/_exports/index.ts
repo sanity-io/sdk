0f1caf2408b5dc916bd256fb86b425d8564fc95f
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2025-06-26 16:31:54 -0400

    feat(react): push up exploratory stuff from today

--- packages/core/src/_exports/index.ts
+++ packages/core/src/_exports/index.ts
@@ -53,8 +53,6 @@ export {
   type DatasetHandle,
   type DocumentHandle,
   type DocumentTypeHandle,
-  type IntentHandler,
-  type IntentHandlers,
   type PerspectiveHandle,
   type ProjectHandle,
   type ReleasePerspective,
@@ -115,17 +113,6 @@ export type {
   TransportEvent,
   UserPresence,
 } from '../presence/types'
-export {
-  checkAndHandleCurrentIntent,
-  handleIntentUrl,
-  type IntentHandlingOptions,
-  type ParsedIntent,
-} from '../intents/intentHandling'
-export {
-  startIntentListener,
-  stopAllIntentListeners,
-  stopIntentListener,
-} from '../intents/intentListener'
 export {getPreviewState, type GetPreviewStateOptions} from '../preview/getPreviewState'
 export type {PreviewStoreState, PreviewValue, ValuePending} from '../preview/previewStore'
 export {resolvePreview, type ResolvePreviewOptions} from '../preview/resolvePreview'
