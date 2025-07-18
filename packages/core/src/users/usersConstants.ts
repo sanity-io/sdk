7aa64799813d0f1fe8ccbadb31c24f5466301a34
Ryan Bonial <ryanbonial@gmail.com>
2025-07-18 15:25:59 -0600

    chore: use more current API version

--- packages/core/src/users/usersConstants.ts
+++ packages/core/src/users/usersConstants.ts
@@ -1,5 +1,5 @@
 // NOTE: currently this API is only available on vX
 export const API_VERSION = 'vX'
-export const PROJECT_API_VERSION = 'v2021-06-07'
+export const PROJECT_API_VERSION = '2025-07-18'
 export const USERS_STATE_CLEAR_DELAY = 5000
 export const DEFAULT_USERS_BATCH_SIZE = 100
