c7c37e02ddf110ded3f81b8895b47b1d943c8d62
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2025-07-17 20:46:21 -0400

    test: ensure server is only manually started in CI

--- apps/kitchensink-react/playwright.config.ts
+++ apps/kitchensink-react/playwright.config.ts
@@ -3,11 +3,13 @@ import {createPlaywrightConfig} from '@repo/e2e'
 export default createPlaywrightConfig({
   testDir: './e2e',
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: process.env['CI']
-      ? 'pnpm build --mode e2e && pnpm preview --mode e2e --port 3333'
-      : 'pnpm dev --mode e2e',
-    reuseExistingServer: true,
-    stdout: 'pipe',
-  },
+  ...(process.env['CI']
+    ? {} // In CI, don't start a webServer since it's started manually
+    : {
+        webServer: {
+          command: 'pnpm dev --mode e2e',
+          reuseExistingServer: true,
+          stdout: 'pipe',
+        },
+      }),
 })
