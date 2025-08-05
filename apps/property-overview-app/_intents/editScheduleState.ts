48456fe02833f7ee81fc579122c4deb7112eb53b
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2025-08-05 09:22:46 -0400

    fix: try to understand why property overview app is busted

---
+++ apps/property-overview-app/_intents/editScheduleState.ts
@@ -0,0 +1,11 @@
+export default {
+  id: 'editScheduleState',
+  action: 'edit',
+  title: 'Edit Schedule State',
+  filters: [
+    {
+      types: ['maintenanceSchedule'],
+    },
+  ],
+  description: 'Update the status/state of a maintenance schedule and view the updated property',
+}
