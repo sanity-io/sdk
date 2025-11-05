cb70f89ebc3ffb90b36d04a959341643860c9fb6
Ryan Bonial <4294665+ryanbonial@users.noreply.github.com>
2025-11-05 16:57:05 -0700

    feat: add agent actions (#654)
    * feat: add agent actions

--- packages/react/src/_exports/sdk-react.ts
+++ packages/react/src/_exports/sdk-react.ts
@@ -6,6 +6,13 @@ export {SanityApp, type SanityAppProps} from '../components/SanityApp'
 export {SDKProvider, type SDKProviderProps} from '../components/SDKProvider'
 export {ComlinkTokenRefreshProvider} from '../context/ComlinkTokenRefresh'
 export {ResourceProvider, type ResourceProviderProps} from '../context/ResourceProvider'
+export {
+  useAgentGenerate,
+  useAgentPatch,
+  useAgentPrompt,
+  useAgentTransform,
+  useAgentTranslate,
+} from '../hooks/agent/agentActions'
 export {useAuthState} from '../hooks/auth/useAuthState'
 export {useAuthToken} from '../hooks/auth/useAuthToken'
 export {useCurrentUser} from '../hooks/auth/useCurrentUser'
