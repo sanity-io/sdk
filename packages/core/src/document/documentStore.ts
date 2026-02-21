6d3a05bbaa9d4905d5c64ef2d725c9f13da9be7c
Carolina Gonzalez <carolina@sanity.io>
2026-02-20 19:15:22 -0500

    feat!: refactor(presence): convert presence to use source (#735)
    BREAKING CHANGE: The SDK now uses source-based APIs in this v3 migration. Existing integrations that relied on projectId/dataset-based assumptions may require updates.
    
    * feat(document): update document store to accept sources
    
    * feat(preview): update preview to use perspective and source
    
    * test: add e2e testing
    
    * feat(releases): update releases store to use sources
    
    * refactor: localize not using resource on client to the client store
    
    * refactor(presence): convert presence to use source
    
    ---------
    
    Co-authored-by: Ryan Bonial <ryanbonial@gmail.com>

--- packages/core/src/document/documentStore.ts
+++ packages/core/src/document/documentStore.ts
@@ -396,7 +396,6 @@ const subscribeToAppliedAndSubmitNextTransaction = ({
       withLatestFrom(
         getClientState(instance, {
           apiVersion: API_VERSION,
-          // TODO: remove in v3 when we're ready for everything to be queried via source
           source: source && !isDatasetSource(source) ? source : undefined,
         }).observable,
       ),
@@ -521,7 +520,6 @@ const subscribeToClientAndFetchDatasetAcl = ({
   key: {source},
 }: StoreContext<DocumentStoreState, BoundSourceKey>) => {
   const clientOptions: ClientOptions = {apiVersion: API_VERSION}
-  // TODO: remove in v3 when we're ready for everything to be queried via source
   if (source && !isDatasetSource(source)) {
     clientOptions.source = source
   }
