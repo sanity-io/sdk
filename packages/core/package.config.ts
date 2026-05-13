7d4c35ed90d6233587ea2c56cfedee32d98cf0eb
Ryan Bonial <ryanbonial@gmail.com>
2026-05-12 21:29:06 -0600

    refactor(config): update rollup-plugin-visualizer import to dynamic loading for ESM compatibility

--- packages/core/package.config.ts
+++ packages/core/package.config.ts
@@ -1,23 +1,25 @@
 import {basePackageConfig} from '@repo/package.config'
 import {defineConfig} from '@sanity/pkg-utils'
-import visualizer from 'rollup-plugin-visualizer'
 
 const enableVisualizer = process.env['VISUALIZER'] === 'true'
+
+// rollup-plugin-visualizer is ESM-only from v7 onward, but @sanity/pkg-utils
+// loads this config file via createRequire(). A static import would fail at
+// build time even when VISUALIZER is unset. Defer to a dynamic import and let
+// Rollup await the resulting plugin promise. The cast is needed because
+// pkg-utils types `rollup.plugins` as Plugin[] though Rollup itself accepts
+// MaybePromise<Plugin> entries.
+const visualizerPlugin = enableVisualizer
+  ? (import('rollup-plugin-visualizer').then(({visualizer}) =>
+      visualizer({filename: './stats/index.html', open: false}),
+    ) as unknown as Plugin)
+  : null
 
 export default defineConfig({
   ...basePackageConfig,
   tsconfig: 'tsconfig.dist.json',
   rollup: {
-    plugins: [
-      ...(enableVisualizer
-        ? [
-            visualizer({
-              filename: './stats/index.html',
-              open: false,
-            }),
-          ]
-        : []),
-    ],
+    plugins: visualizerPlugin ? [visualizerPlugin] : [],
   },
   dts: 'rolldown',
 })
