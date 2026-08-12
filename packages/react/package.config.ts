f375af768b6a747bf0a05aa8de69fbacc39af315
Ryan Bonial <ryanbonial@gmail.com>
2026-08-12 12:32:53 -0600

    build: replace rollup-plugin-visualizer with rolldown bundle analyzer

--- packages/react/package.config.ts
+++ packages/react/package.config.ts
@@ -1,8 +1,5 @@
-import {basePackageConfig} from '@repo/package.config'
+import {basePackageConfig, bundleAnalyzerPlugins} from '@repo/package.config'
 import {defineConfig} from '@sanity/pkg-utils'
-import visualizer from 'rollup-plugin-visualizer'
-
-const enableVisualizer = process.env['VISUALIZER'] === 'true'
 
 export default defineConfig({
   ...basePackageConfig,
@@ -10,14 +7,5 @@ export default defineConfig({
   reactCompiler: {
     target: '18',
   },
-  plugins: [
-    ...(enableVisualizer
-      ? [
-          visualizer({
-            filename: './stats/index.html',
-            open: false,
-          }),
-        ]
-      : []),
-  ],
+  plugins: bundleAnalyzerPlugins(import.meta.url),
 })
