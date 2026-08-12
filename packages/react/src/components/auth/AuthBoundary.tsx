aadbaaa470b74c1a4e68003bc18ebd0f5a146004
Carolina Gonzalez <carolina@sanity.io>
2026-08-12 07:34:46 -0400

    fix!: split other utils into subdomain and internal exports (#1098)

--- packages/react/src/components/auth/AuthBoundary.tsx
+++ packages/react/src/components/auth/AuthBoundary.tsx
@@ -1,5 +1,6 @@
 import {CorsOriginError} from '@sanity/client'
-import {AuthStateType, getCorsErrorProjectId, isImportError, isStudioConfig} from '@sanity/sdk'
+import {AuthStateType, getCorsErrorProjectId, isImportError} from '@sanity/sdk'
+import {isStudioConfig} from '@sanity/sdk/_internal'
 import {useEffect, useMemo} from 'react'
 import {ErrorBoundary, type FallbackProps} from 'react-error-boundary'
 
