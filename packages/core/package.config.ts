a74b9c359cf6c3614214fd49894629546b9878e9
Ryan Bonial <4294665+ryanbonial@users.noreply.github.com>
2026-08-13 10:08:12 -0600

    build: replace rollup-plugin-visualizer with rolldown bundle analyzer (#1101)
    * build: replace rollup-plugin-visualizer with rolldown bundle analyzer

--- packages/core/package.config.ts
+++ packages/core/package.config.ts
@@ -1,20 +1,8 @@
-import {basePackageConfig} from '@repo/package.config'
+import {basePackageConfig, bundleAnalyzerPlugins} from '@repo/package.config'
 import {defineConfig} from '@sanity/pkg-utils'
-import visualizer from 'rollup-plugin-visualizer'
-
-const enableVisualizer = process.env['VISUALIZER'] === 'true'
 
 export default defineConfig({
   ...basePackageConfig,
   tsconfig: 'tsconfig.dist.json',
-  plugins: [
-    ...(enableVisualizer
-      ? [
-          visualizer({
-            filename: './stats/index.html',
-            open: false,
-          }),
-        ]
-      : []),
-  ],
+  plugins: bundleAnalyzerPlugins(import.meta.url),
 })
